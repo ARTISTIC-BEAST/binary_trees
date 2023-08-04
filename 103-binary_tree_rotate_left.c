6826dce5c6d8bd13171f0ed55939fe7c32454309
ARTISTIC-BEAST <artisticbeast1@gmail.com>
2023-08-03 17:11:08 -0700

    103-binary_tree_rotate_left.c

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -3,7 +3,7 @@
 /**
 * binary_tree_rotate_left - performs a left-rotation on a binary tree
 * @tree: pointer to the root node of the tree to rotate
-* Return: a pointer to the new root node of the tree once rotated
+* Return: a pointer to the new root node of the tree once rotated.
 * NULL if tree is NULL.
 **/
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
